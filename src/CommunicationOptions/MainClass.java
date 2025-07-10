daecc29e164a88d1174e4e739d44906444970081
imjinan <ijinankareem@gmail.comgit config --global use.email ijinankareem@gmail.com>
2025-07-10 02:09:43 -0400

    second commit

--- src/CommunicationOptions/MainClass.java
+++ src/CommunicationOptions/MainClass.java
@@ -2,6 +2,10 @@ package CommunicationOptions;
 
 public class MainClass {
     public static void main(String[] args) {
+        if(args.length==0){
+            System.out.println("enter valid input");
+            return;
+        }
         String input=args[0];
         Communication c1;
         switch (input.toLowerCase()) {
